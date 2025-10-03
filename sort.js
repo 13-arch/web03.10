763976ddc6eeb3d827a90060cef6a1a6a26334f3
13-arch <Iron_Mike@tutamail.com>
2025-10-03 13:30:08 +0300

    Add files via upload

--- sort.js
+++ sort.js
@@ -1,1 +1,9 @@
+const allRows = document.querySelectorAll('tr')
+const allColum = document.querySelectorAll('td')
+
+console.log(allColum)
+console.log(allRows)
 let name = []
+for(let i =0; i<allRows; i++){
+    
+}
